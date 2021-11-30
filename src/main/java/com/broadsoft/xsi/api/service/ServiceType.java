cc2d8e54db8d8fa7abea64c2e795afc05f7e438c
Stefan Prelle <spr@plusnet.de>
2021-11-30 15:45:24 +0100

    Already moved some code from XSI

---
+++ src/main/java/com/broadsoft/xsi/api/service/ServiceType.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.broadsoft.xsi.api.service;
+
+/**
+ * @author spr
+ *
+ */
+public enum ServiceType {
+
+	BLF,
+	CALL_CENTER,
+	CALL_FORWARDING_ALWAYS,
+	CALL_LOGS,
+	CALL_RECORDING,
+	COLLABORATE,
+	CUSTOM_CONTACT,
+	PROFILE,
+	REMOTE_OFFICE,
+	
+	SERVICES,
+	
+}
